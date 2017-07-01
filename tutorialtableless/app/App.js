8e7196053a07d5f3de5607cbeccbb532cd1abf16
ruby <e>
2017-07-01 02:32:21 -0300

    tutorial do tableless completo

---
+++ tutorialtableless/app/App.js
@@ -0,0 +1,6 @@
+var React = require('react')
+var reactDom = require('react-dom')
+
+var MeuComponente = require('./components/meuComponente.js')
+
+reactDom.render(meuComponente, document.getElementById('app'))
---
+++ tutorialtableless/app/components/meuComponente.js
@@ -0,0 +1,10 @@
+var React = require('react')
+
+var MeuComponente = React.createClass({
+  render: function() {
+    return(
+      <h1>teste</h1>
+    )
+  }
+})
+module.exports = MeuComponente
